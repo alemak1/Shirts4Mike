685d537eb2949488704b70536d8c541a29639051
yongrenwang <czie.alexmak@gmail.com>
2017-01-11 16:16:09 +0800

    Created a PDO object in our database.php file, with error handling too

--- database.php
+++ database.php
@@ -1,4 +1,12 @@
 <?php
 
-$db = new PDO("mysql:host=localhost;dbname=shirts4mike;port=3306","root","root");
-var_dump($db);
+try{
+	$db = new PDO("mysql:host=localhost;dbname=shirts4mike;port=3306","root","root");
+	var_dump($db);
+}catch(Exception $e){
+
+	echo "Could not connect to the database";
+	exit;
+}
+
+echo "Woo hoo!";
