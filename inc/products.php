65bd5c108bfe5d3e615852002b8d246c3ac4141d
yongrenwang <czie.alexmak@gmail.com>
2017-01-11 18:01:55 +0800

    Replaced the database connection code in get_products_all() with a require statement that imports the code from the database.php file

--- inc/products.php
+++ inc/products.php
@@ -309,15 +309,7 @@ function get_products_all() {
     }
     */
 
-    try{
-        $db = new PDO("mysql:host=localhost;dbname=shirts4mike;port=3306","root","root");
-        $db->exec("SET NAMES 'utf8'");
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }catch(Exception $e){
-
-        echo "Could not connect to the database";
-        exit;
-    }
+    REQUIRE(ROOT_PATH . "inc/database.php");
 
     try{
         $results = $db->query("SELECT name,price, img,sku,paypal FROM products ORDER BY sku ASC");
