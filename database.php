4de94dbfb783e34602a38c5879afaa6ff55f58aa
yongrenwang <czie.alexmak@gmail.com>
2017-01-11 16:38:56 +0800

    Performed a simple query on PDO object for all items in the database

--- database.php
+++ database.php
@@ -2,6 +2,8 @@
 
 try{
 	$db = new PDO("mysql:host=localhost;dbname=shirts4mike;port=3306","root","root");
+	$db->exec("SET NAMES 'utf8'");
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	var_dump($db);
 }catch(Exception $e){
 
@@ -9,4 +11,10 @@ try{
 	exit;
 }
 
-echo "Woo hoo!";
+try{
+	$results = $db->query("SELECT name,price FROM products ORDER BY sku ASC");
+	echo "Our query ran successfully!";
+}catch(Exception $e){
+	echo "Data could not be found";
+	exit;
+}
