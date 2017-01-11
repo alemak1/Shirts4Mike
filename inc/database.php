c77d78d3b1e569d6810a17c9de07c9a9db342a93
yongrenwang <czie.alexmak@gmail.com>
2017-01-11 18:16:41 +0800

    Refactored code so that authentication credentials are stored in global constants defined in the configuration file; database connection code is database.php file that is in the include folder, so that database connection code can be imported into functions via require and include statements

--- inc/database.php
+++ inc/database.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-	$db = new PDO("mysql:host=localhost;dbname=shirts4mike;port=3306","root","root");
+	$db = new PDO("mysql:host=". DB_HOST .";dbname=" . DB_NAME .";port=" . DB_PORT,DB_USER,DB_PASS);
 	$db->exec("SET NAMES 'utf8'");
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(Exception $e){
